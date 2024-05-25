3122c13000ec2a8cd1d51df9c0ea29b189137306
siperzen <156843337+siperzen@users.noreply.github.com>
2024-05-25 12:09:26 +0500

    Add random RGB color when hovering over a square

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const button = document.querySelector("button");
+const container = document.querySelector(".container");
+
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
 
 function addGrid(number) {
     for(let i = 0; i < number; i++) {
@@ -9,6 +22,7 @@ function addGrid(number) {
        for(let i = 0; i < number; i++) {
         const column = document.createElement("div");
         column.classList.add("column");
+
         row.appendChild(column);
 
         const squareHeight = 480 / number;
@@ -16,17 +30,24 @@ function addGrid(number) {
         column.style.height = `${squareHeight}px`;
 
         column.addEventListener("mouseover", () => {
-            column.style.backgroundColor = "yellow";
+            column.style.backgroundColor = randomRgbColor();
         })
     }
     }
 }
 
-const button = document.querySelector("button");
-const container = document.querySelector(".container")
+
 button.addEventListener("click", () => {
-    let number = prompt("Enter your desired grid size.");
+    let number = prompt("Enter your desired grid size (Number).");
+    if(number > 100) {
+        alert("Too big, type a number less than 100")
+    }
+    else if(number < 1) {
+        alert("Enter a positive number.");
+    }
+    else {
     container.innerHTML = "";
     addGrid(number);
+};
 })
 
